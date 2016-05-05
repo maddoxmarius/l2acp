771fe3f659a670566e458bfee7f1fc1d6b937bc4
Marius Bordeianu <marius.bordeianu@hpe.com>
2016-05-05 10:43:07 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,25 @@
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc index
+	 * @name app
+	 * @description
+	 * # app
+	 *
+	 * Main module of the application.
+	 */
+
+	angular.module('l2acp', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngCookies',
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router',
+		'home',
+	]);
+
+})();
---
+++ app/modules/home/homeCtrl.js
@@ -0,0 +1,32 @@
+(function () {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.controller:HomeCtrl
+	 * @description
+	 * # HomeCtrl
+	 * Controller of the app
+	 */
+
+	angular
+		.module('l2acp')
+		.controller('HomeCtrl', Home);
+
+	Home.$inject = ['homeService'];
+
+	/*
+	 * recommend
+	 * Using function declarations
+	 * and bindable members up top.
+	 */
+
+	function Home(homeService) {
+		/*jshint validthis: true */
+		var vm = this;
+		vm.title = "Hello, l2acp!";
+		vm.version = "1.0.0";
+		vm.listFeatures = homeService.getFeaturesList();
+	}
+
+})();
